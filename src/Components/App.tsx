8f21c334285d4d58b32883a881492d064331f19b
JamesSleep <tyuyiy@naver.com>
2020-10-03 05:05:56 +0900

    #3.2 CSS in React part Three

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Header from './Header/';
 import Router from './Router';
 
 const App = () => {
   return (
     <>
-      <Header />
       <Router />
     </>
   );
