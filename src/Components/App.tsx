8e3dd2787ad11cd9c65846d47b91be3fbb89bffe
JamesSleep <tyuyiy@naver.com>
2020-10-03 05:22:03 +0900

    #3.3 GlobalStyles and Header

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Router from './Router';
+import GlobalStyels from "./GlobalStyles";
 
 const App = () => {
   return (
     <>
       <Router />
+      <GlobalStyels />
     </>
   );
 }
