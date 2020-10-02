aec57fc33c23cdd15b501855e2c4ae8e2efef917
JamesSleep <tyuyiy@naver.com>
2020-10-03 04:38:43 +0900

    #2.2 React Rotuer part Two

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from './Header';
 import Router from './Router';
 
 const App = () => {
   return (
-    <><Router /></>
+    <>
+      <Header />
+      <Router />
+    </>
   );
 }
 
