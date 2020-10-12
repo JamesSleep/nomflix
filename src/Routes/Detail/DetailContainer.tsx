ff6236db51a08b8aa64fc2e5b15bf367a5d45b2c
JamesSleep <tyuyiy@naver.com>
2020-10-12 21:29:21 +0900

    #5.5 Detail Container part One

--- src/Routes/Detail/DetailContainer.tsx
+++ src/Routes/Detail/DetailContainer.tsx
@@ -1,13 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { RouteComponentProps } from "react-router-dom";
 import { DetailProps } from "../../Interfaces";
 import DetailPresenter from "./DetailPresenter";
 
-export default () => {
+interface RouteParams {
+  id: string;
+}
+
+export default (props: RouteComponentProps<RouteParams>) => {
   const [data, setData] = useState<DetailProps>({
     result: null,
     loading: true,
     error: null
   });
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const { 
+      match: {
+        params: { id }
+      },
+      history: { push }
+    } = props;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push("/");
+    }
+  }
+
   return (
     <DetailPresenter 
     result={data.result}
